c192aab95f33a320604c05cfad4af62e9d4ff44f
sofiafuertes <sofia_fuertes@hotmail.com>
2024-10-16 15:50:18 +0200

    .

--- modele/modele_form.php
+++ modele/modele_form.php
@@ -1,23 +1,20 @@
 <?php
 
 class Model_Form {
-    private ?string $name;
+    private ?string $first_name;
+    private ?string $last_name;
     private ?string $mail;
-    private ?string $message;
+    private ?string $password;
+    private ?string $passwordConfirm;
 
-    public function __construct(?string $mail){
+
+    public function __construct(?string $mail, ?string $password){
         $this->mail = $mail;
+        $this->password = $password;
     }
 
 //* Getters and Setters   
-    public function getName(): ?string {
-        return $this->name;
-    }
-    public function setName(?string $name): self {
-        $this->name = $name;
-        return $this;
-    }
-    public function getMail(): ?string {
+        public function getMail(): ?string {
         return $this->mail;
     }
     public function setMail(?string $mail): self {
@@ -31,4 +28,25 @@ class Model_Form {
         $this->message = $message;
         return $this;
     }    
+    public function getFirstName(): ?string {
+        return $this->first_name;
+    }
+    public function setFirstName(?string $first_name): self {
+        $this->first_name = $first_name;
+        return $this;
+    }
+    public function getPassword(): ?string {
+        return $this->password;
+    }
+    public function setPassword(?string $password): self {
+        $this->password = $password;
+        return $this;
+    }
+    public function getPasswordConfirm(): ?string {
+        return $this->passwordConfirm;
+    }
+    public function setPasswordConfirm(?string $passwordConfirm): self {
+        $this->passwordConfirm = $passwordConfirm;
+        return $this;
+    }
 }
